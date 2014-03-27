2c0475bc989a5693b7d12593ce98089886cc2713
Kyle Meier <kyledmeier@gmail.com>
2014-03-26 17:13:06 -0700

    text validation and workout styling

--- js/script.js
+++ js/script.js
@@ -1,9 +1,55 @@
-var exercise2 = $('#exercise2').offset().top-65;
-var nextMove = exercise2;
+var movementDistance = 482;
+var nextMove = movementDistance;
+var currentStep = 1;
+var doneShift = $('#workout').offset().top-50;
+var exercise1Name = '';
+var exercise2Weight = '';
+var exercise1SetsxReps = '';
+
+
+var noRefresh = function(){
+	return false;
+}
+//if shift is off, change above nubmer to the number shown here:
+//console.log($('#exercise2pick').offset().top - $('#exercise1pick').offset().top);
 
 $('.nextexercisebutton').click(function(){
-    $('#exercises').animate({
-    	bottom:nextMove}, {duration:'slow', queue:false}); 
-    	nextMove += exercise2;   	
-    	return false;
+	// var empty = $(this).parent().find("input").filter(function() {
+ //       	return this.value === "";
+ //   		});
+ //   		if(empty.length) {
+ //       		console.log("No good!");
+ //   		}else{
+ //   			console.log("Good to go!");
+    		$('#slidingcontent').animate({bottom:nextMove}, {duration:'slow', queue:false}); 
+   			nextMove += movementDistance;
+   			currenteStep++;  	
+   			return false;
+   		// }
 });
+
+$('.donebutton').click(function(){
+			// var empty = $(this).parent().find("input").filter(function() {
+  //       	return this.value === "";
+  //   		});
+  //   		if(empty.length) {
+  //       		console.log("No good!");
+  //   		}else{
+  //   			console.log("Good to go!");
+	exercise1Name = $("#exercise1name").val();
+   	$("#exercise1workoutname").replaceWith( "<span id=\"exercise1workoutname\">"+exercise1Name+"</span>");
+	exercise1Weight = $("#exercise1weight").val();
+   	$("#exercise1workoutweight").replaceWith( "<span id=\"exercise1workoutname\">"+exercise1Weight+"</span>");   	
+	exercise1SetsxReps = $("#exercise1setsxreps").val();
+   	$("#exercise1workoutsetsxreps").replaceWith( "<span id=\"exercise1workoutsetsxreps\">"+exercise1SetsxReps+"</span>");
+
+    $('#slidingcontent').animate({bottom:doneShift}, {duration:'slow', queue:false});   	
+    	return false;
+    	//}
+});
+
+$('.timer').click(function(){
+	console.log("Hey");
+}
+
+
