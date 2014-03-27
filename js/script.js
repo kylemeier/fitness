14c1638056852dd02a03d1113f09eb2b89defdc3
Kyle Meier <kyledmeier@gmail.com>
2014-03-26 18:36:38 -0700

    created countdown timer

--- js/script.js
+++ js/script.js
@@ -49,7 +49,44 @@ $('.donebutton').click(function(){
 });
 
 $('.timer').click(function(){
-	console.log("Hey");
+  var totalTime = 5;
+  
+  $('#clock').html(timeCalc(totalTime));
+
+  var countDown = setInterval(function(){
+    totalTime--;
+    
+    $('#clock').html(timeCalc(totalTime));
+
+    if(totalTime == 0){
+      window.navigator.vibrate(200);
+      clearInterval(countDown);
+    }
+  },1000);
+
+  
+});
+
+var timeCalc = function(totalTime){
+
+  var minutes = Math.floor(totalTime/60);
+  var seconds = Math.round(totalTime % 60);
+  var minutesStr='';
+  var secondsStr='';
+
+  if(minutes<10){
+    minutesStr='0'+minutes;
+  }else{
+    minutesStr = minutes;
+  }
+  if(seconds<10){
+    secondsStr='0'+seconds;
+  }else{
+    secondsStr = seconds;
+  }
+
+  return(minutesStr+":"+secondsStr);
+
 }
 
 
