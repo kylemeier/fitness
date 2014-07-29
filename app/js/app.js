084365f0ef8b892d940056bcf810487ed4212393
Kyle Meier <kyledmeier@gmail.com>
2014-07-29 11:30:07 -0700

    styling for desktop view

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,25 @@
 //To fix:
 //creating a user doesn't retain state on refresh
+//user can delete group name in edit group
 //
 //UI issues:
 //loading icon on create user
+//loading icon on initial load
 //
 //iOS:
 //login buttons look wonky in landscape
 //
 //To do:
 //move resolve code into a service
-//Add weight change notification to workout screen -- under name -- next weight: ##lbs (##lbs change) -- create variable for weight change
-//store workouts
+//clean up CSS file
+//comment code
 //
 //Desktop view:
 //header color spans entire width, content is much smaller, need to determine max width
 //maybe more pronounced borders on content?
 //boxes on groups page need to be inset
 //
-//Additional Features:
+//Possible ideas:
 //after two failures, decrease weight by 5lbs and increase reps by 2 per set
 //after three failures, show alert asking if user wants to set a reminder to change the exercise or reset the count
 //reminder will be an exclamation point in place of the delete button
